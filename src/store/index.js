9a2c67efac51eb4c10bd23547f63fe59461b9796
zk <596718356@qq.com>
2020-03-04 15:23:21 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import mutations from './mutations'
+import actions from './actions'
+import state from './state'
+import user from './module/user'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state,
+  mutations,
+  actions,
   modules: {
+    user
   }
 })
